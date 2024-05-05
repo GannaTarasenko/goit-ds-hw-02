7f6a874723acd5eddadfc96402c0115ac2f1b00b
GannaTarasenko <159177939+GannaTarasenko@users.noreply.github.com>
2024-05-05 09:14:39 +0200

    заповнення таблиць

--- seed.py
+++ seed.py
@@ -1,42 +1,37 @@
+from sqlite3 import Error
+import sqlite3
 from faker import Faker
-import psycopg2
 import random
 
-# Підключення до бази даних
-conn = psycopg2.connect(
-    dbname="your_database",
-    user="your_user",
-    password="your_password",
-    host="your_host",
-    port="your_port"
-)
+"""Підключення до бази даних"""
+conn = sqlite3.connect("your_database.db")
 
-# Створення курсора
+"""Створення курсора"""
 cur = conn.cursor()
 
-# Ініціалізація Faker
+"""Ініціалізація Faker"""
 fake = Faker()
 
-# Заповнення таблиці users
+"""Заповнення таблиці users"""
 for _ in range(10):
     fullname = fake.name()
     email = fake.email()
-    cur.execute("INSERT INTO users (fullname, email) VALUES (%s, %s)", (fullname, email))
+    cur.execute("INSERT INTO users (fullname, email) VALUES (?, ?)", (fullname, email))
 
-# Заповнення таблиці status
+"""Заповнення таблиці status"""
 statuses = ['new', 'in progress', 'completed']
 for status in statuses:
-    cur.execute("INSERT INTO status (name) VALUES (%s)", (status,))
+    cur.execute("INSERT INTO status (name) VALUES (?)", (status,))
 
-# Заповнення таблиці tasks
+"""Заповнення таблиці tasks"""
 for _ in range(20):
     title = fake.text(max_nb_chars=100)
     description = fake.text()
     status_id = random.randint(1, len(statuses))
     user_id = random.randint(1, 10)
-    cur.execute("INSERT INTO tasks (title, description, status_id, user_id) VALUES (%s, %s, %s, %s)",
+    cur.execute("INSERT INTO tasks (title, description, status_id, user_id) VALUES (?, ?, ?, ?)",
                 (title, description, status_id, user_id))
 
-# Підтвердження змін та закриття з'єднання
+"""Підтвердження змін та закриття з'єднання"""
 conn.commit()
 conn.close()
